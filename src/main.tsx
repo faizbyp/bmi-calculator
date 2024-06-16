ce2b5270b3ec6c96427f5fc627582816749dd828
faizbyp <faizbyp@gmail.com>
2024-06-16 16:44:44 +0700

    mui: basics and theming

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#7689de",
+    },
+    secondary: {
+      main: "#a9dce3",
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: "3rem",
+      fontWeight: "600",
+    },
+    h2: {
+      fontSize: "1.75rem",
+      fontWeight: "600",
+    },
+    h3: {
+      fontSize: "1.5rem",
+      fontWeight: "600",
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
